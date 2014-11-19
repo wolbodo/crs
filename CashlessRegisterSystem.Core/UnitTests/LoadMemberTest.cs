3d987fd1c19004ed7b19be1fe7035477765f02a8
unknown <bwb@bwblabs.com>
2014-11-19 21:21:21 +0100

    Fixing sync issues & other stuff

--- CashlessRegisterSystem.Core/UnitTests/LoadMemberTest.cs
+++ CashlessRegisterSystem.Core/UnitTests/LoadMemberTest.cs
@@ -26,10 +26,6 @@ namespace CashlessRegisterSystemCore.UnitTests
         private List<string> memberLines;
         private string membersText = @"#Name[string];KeyCode[(hex)string];PaymentMethod[enum:Incasso|PrePaid];IncassoLevel[int in cents];AccountNumber[string];AccountName[string];BalanceDate[date:yyyy-MM-dd];Balance[int in cents]
 #Wikkert;A1B2C3;Incasso;50000;123456;W. Olbodo;2013-01-14;1250
-#This means: Wikkert has key A1B2C3; pays with Incasso (level to € 50.00 positive); has the bank account 123456 with the name W. Olbodo; last time (2013-01-14) he had a balace of ? 12.50;;;;
-Amber;??????;PrePaid;0;4399335;A.M. Leeman;2012-07-23;3404
-BWB;??????;Incasso;5000;390362913;B.W. Broersma;2012-07-23;5000
-Eettafel;??????;PrePaid;0;;;2012-07-23;0
-Emiel;??????;PrePaid;0;119102528;E. Verstegen;2012-07-23;-13426";
+#This means: Wikkert has key A1B2C3; pays with Incasso (level to € 50.00 positive); has the bank account 123456 with the name W. Olbodo; last time (2013-01-14) he had a balace of ? 12.50;;;;";
     }
 }
