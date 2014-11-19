a1bdf9126cffafebecb5b3b9304621f51ebb9485
unknown <bwb@bwblabs.com>
2014-11-19 21:21:41 +0100

    Fixing sync issues & other stuff

--- CashlessRegisterSystem.Core/UnitTests/TransferTest.cs
+++ CashlessRegisterSystem.Core/UnitTests/TransferTest.cs
@@ -25,12 +25,6 @@ namespace CashlessRegisterSystemCore.UnitTests
         }
 
         private string[] transferLines;
-        private string transferText = @"#Transfer voor maand: 2012-11
-2012-11-29;Djoeke;50;Bank; ANDEREN Mw D S T Ardon ANDEREN           contributie!!                    transactiedatum: 29-11-2012
-2012-11-27;Amber;50;Incasso; DELFT A M LEEMAN DELFT                 transactiedatum: 27-11-2012
-2012-11-26;Jitske;115;Cash; Mw J N Folkertsma BELGIE         rest afbetaling.                 transactiedatum: 26-11-2012
-2012-11-21;Remy;300;Bonnetje; DELFT Hr R van Rooijen DELFT           schuldenlijst                    transactiedatum: 21-11-2012
-2012-11-12;Judith;75;Contributie; SCHULD JUDITH
-2012-11-12;Annette;50;Activiteiten; AFLOSSING SCHULD OKTOBER +       VOORSCHOT";
+        private string transferText = @"#Transfer voor maand: 2012-11";
     }
 }
